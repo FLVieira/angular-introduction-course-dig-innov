eb1a8398e995ba8ee35e25ef99213b00e3e4dded
Fábio Vieira <fabioluizvieiraf@gmail.com>
2021-04-10 23:41:10 -0300

    segregating the courses module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
-import { StarComponent } from './star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { CourseInfoComponent } from './course-info/course-info.component';
 import { Page404 } from './page404/page404.component';
+import { CourseModule } from './courses/courses.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
     NavBarComponent,
     Page404,
-    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
+    CourseModule,
     RouterModule.forRoot([
-      {
-        path: 'courses',
-        component: CourseListComponent,
-      },
-      {
-        path: 'courses/info/:id',
-        component: CourseInfoComponent
-      },
       {
         path: '',
         redirectTo: 'courses',
---
+++ src/app/courses/courses.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+
+import { CourseInfoComponent } from "../course-info/course-info.component";
+import { CourseListComponent } from "./course-list.component";
+import { StarComponent } from '../star/star.component';
+import { ReplacePipe } from '../pipe/replace.pipe';
+
+@NgModule({
+  declarations: [
+    CourseListComponent,
+    CourseInfoComponent,
+    ReplacePipe,
+    StarComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      {
+        path: 'courses',
+        component: CourseListComponent,
+      },
+      {
+        path: 'courses/info/:id',
+        component: CourseInfoComponent
+      },
+    ])
+  ]
+})
+export class CourseModule {
+
+}
